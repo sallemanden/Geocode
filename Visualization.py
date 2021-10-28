dfd074e73268d0729eaca0da78b53177d66727b2
Simon Sall Andersen <ssalla@ruc.dk>
2021-10-28 20:54:11 +0200

    ADDED NEW LOCATIONS AND CORRECTED COMMENTS

--- Visualization.py
+++ Visualization.py
@@ -2,23 +2,23 @@ import pandas as pd
 import folium
 from folium.plugins import MarkerCluster
 
-# Start the map at center of roskilde
-m = folium.Map(location=[55.64152,12.08035], tiles ='OpenStreetMap', zoom_start = 13)
+# DISPLAY THE MAP
+map = folium.Map(location=[55.64152,12.08035], tiles ='OpenStreetMap', zoom_start = 13)
 
-# Install one static marker as Post Office, we do not have this location in our  locationGeo.csv since we are adding new post officies
-folium.Marker(location=[55.65233,12.13799], popup='POSTHUS', icon = folium.Icon(color='red')).add_to(m)
+# CREATE A STATIC MARKER AT STATIC LOCATION
+folium.Marker(location=[55.65233,12.13799], popup='POSTOFFICE', icon = folium.Icon(color='red')).add_to(map)
 
-# read the location geo CSV file
+# READ THE CSV FILE
 csv = pd.read_csv('locationGeo.csv')
 
-# Go over every single row in our CSV file and take latitude and longitiude as well as street
+# ILTERATE THROUGH THE CSV FILE AND ADD THE DATA TO THREE VARIABLES
 for i, row in csv.iterrows():
     street = csv.at[i,'street']
     lat = csv.at[i,'lat']
     lng = csv.at[i,'lng']
 
-    # Use the above mentioned variables to create a marker on the map, latitude and longitiude is used to determine the location, while street gives valueable information.
-    folium.Marker(location=[lat,lng], popup=street, icon = folium.Icon(color='blue')).add_to(m)
+    # CREATE A MARKER FOR EVERY ADDRESS IN THE CSV FILE
+    folium.Marker(location=[lat,lng], popup=street, icon = folium.Icon(color='blue')).add_to(map)
 
-# save the markers in the index.html which is where we display the map.
-m.save('index.html')
+# ADD THE MAP TO INDEX.HTML FILE
+map.save('index.html')
--- getGeo.py
+++ getGeo.py
@@ -2,29 +2,26 @@ import pandas as pd
 import requests
 import json
 
-#Read CSV file
+# READ CSV FILE
 csv = pd.read_csv("locations.csv")
 
-
-# Loop through every single row of the CSV file, take street, zip and country as the apiAddress we utilize as parameter
+# USE API RESPONSE TO FIND LATITUDE AND LONGITUDE
+# ILTERATE THROUGH EVERY SINGLE ROW IN THE CSV FILE AND ADD TO LAT AND LNG
 for i, row in csv.iterrows():
     address = str(csv.at[i,'street']) + ',' + str(csv.at[i,'zip']) + ',' + str(csv.at[i,'country'])
 
     parameters = {
-    # location is = to the variable since we loop through our csv file
         "key" : "btT47svcW8jcWNEAIAGNlrXgiv0Orxmw",
         "location" : address
     }
-    # Only get latitude and longitiude
-    resp = requests.get("http://www.mapquestapi.com/geocoding/v1/address", params = parameters)
-    data = json.loads(resp.text)['results']
+    response = requests.get("http://www.mapquestapi.com/geocoding/v1/address", params = parameters)
+    data = json.loads(response.text)['results']
 
     lng = data[0]['locations'][0]['latLng']['lng']
     lat = data[0]['locations'][0]['latLng']['lat']
 
-
-    csv.at[i,'lat'] = lat
     csv.at[i,'lng'] = lng
+    csv.at[i,'lat'] = lat
 
- # Make a new CSV File with the latitude and longitiude values
+ # CREATE A NEW CSV FILE WITH THE LONGITUDE AND LATITUDE
 csv.to_csv('locationGeo.csv')
